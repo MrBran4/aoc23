92bf781908e155f8f80426615b7652f22b705a23
Brandon Randle <branr62@gmail.com>
2023-12-01 16:20:50 +0000

    (optimisation) check numbers in length order

--- day1/part2.php
+++ day1/part2.php
@@ -2,16 +2,16 @@
 
 // Array of stringy numbers, indexed by the number they represent.
 const SPELLINGS = [
-    "zero",
-    "one",
-    "two",
-    "three",
-    "four",
-    "five",
-    "six",
-    "seven",
-    "eight",
-    "nine"
+    1 => "one",
+    2 => "two",
+    6 => "six",
+    4 => "four",
+    5 => "five",
+    9 => "nine",
+    0 => "zero",
+    3 => "three",
+    7 => "seven",
+    8 => "eight"
 ];
 
 /**
@@ -19,17 +19,18 @@ const SPELLINGS = [
  * @param string $in The string to check
  * @return int The number at the start of the string, or -1 if there is none.
  */
-function number_at_start(string $in): int {
+function number_at_start(string $in): int
+{
     // Can't be a number if it's less than 3 chars
-    if( strlen($in) < 3)
+    if (strlen($in) < 3)
         return -1;
 
     // Check each number in the array
-    foreach(SPELLINGS as $idx => $spelling){
+    foreach (SPELLINGS as $intval => $spelling) {
 
         // If the string starts with the spelling, return the index
         if (0 === strpos($in, $spelling))
-            return $idx;
+            return $intval;
 
     }
 
@@ -40,12 +41,12 @@ function number_at_start(string $in): int {
 $total = 0;
 
 // Process the input line by line
-while (FALSE !== ($line = fgets(STDIN))){
+while (FALSE !== ($line = fgets(STDIN))) {
     $first = NULL;
     $last = NULL;
 
     // Loop through each char
-    foreach( str_split($line) as $idx => $char){
+    foreach (str_split($line) as $idx => $char) {
         $thisNum = -1;
 
         // If it's a number, treat it as such and update the vars
