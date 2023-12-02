ce144db4764f225a9f29cce5b67ec7dc555e5c34
Brandon Randle <branr62@gmail.com>
2023-12-02 21:00:27 +0000

    day 2

---
+++ day2/go.mod
@@ -0,0 +1,5 @@
+module github.com/mrbran4/aoc23
+
+go 1.21.3
+
+require github.com/davecgh/go-spew v1.1.1
---
+++ day2/part2/main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+const (
+	maxReds   = 12
+	maxGreens = 13
+	maxBlues  = 14
+)
+
+var gameRegex = regexp.MustCompile(`Game (\d+):`)
+var redRegex = regexp.MustCompile(`(\d+) red`)
+var greenRegex = regexp.MustCompile(`(\d+) green`)
+var blueRegex = regexp.MustCompile(`(\d+) blue`)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+
+	total := 0
+
+	for {
+		// Read line by line until EOF
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			break
+		}
+
+		gameNum := gameRegex.FindStringSubmatch(line)
+		if len(gameNum) != 2 {
+			continue
+		}
+
+		// The regex matches integers so the conversion is safe
+		gameNumInt, _ := strconv.Atoi(gameNum[1])
+
+		fmt.Printf("\nGame %d\n", gameNumInt)
+
+		gamesPart := strings.Split(line, ":")[1]
+
+		var (
+			minReds   = 0
+			minGreens = 0
+			minBlues  = 0
+		)
+
+		// Return early if there are too many of any color
+		for _, game := range strings.Split(gamesPart, ";") {
+			var (
+				reds   = extractColor(redRegex, game)
+				greens = extractColor(greenRegex, game)
+				blues  = extractColor(blueRegex, game)
+			)
+
+			fmt.Printf("Game %d: %d red, %d green, %d blue\n", gameNumInt, reds, greens, blues)
+
+			minReds = max(minReds, reds)
+			minGreens = max(minGreens, greens)
+			minBlues = max(minBlues, blues)
+		}
+
+		total = total + (minReds * minGreens * minBlues)
+	}
+
+	fmt.Println(total)
+}
+
+func extractColor(rx *regexp.Regexp, game string) int {
+	parts := rx.FindStringSubmatch(game)
+
+	if len(parts) != 2 {
+		return 0
+	}
+
+	// the regex matches integers so the conversion is safe
+	intval, _ := strconv.Atoi(parts[1])
+
+	return intval
+}
